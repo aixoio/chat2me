e4f37e8d79619be1fa6b91dd11f12c83853117a2
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-10-07 20:17:08 +1100

    Update dependencies, refactor RSA helper, remove SHA512 helper and keystore file.

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,16 @@
 module github.com/aixoio/chat2me/server
 
 go 1.23.0
+
+require (
+	github.com/ProtonMail/gopenpgp/v3 v3.0.0-beta.1-proton
+	github.com/google/uuid v1.6.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v1.1.0-beta.0-proton // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/aixoio/chat2me/server/keystore"
+	"github.com/aixoio/chat2me/utils/input"
+	"github.com/aixoio/chat2me/utils/keystore"
 )
 
 func main() {
   fmt.Println("Checking for database...")
   if !keystore.CheckIfKeyStoreDBExists() {
+    keypass, err := input.Ask("Choose a password for your keys:")
+    if err != nil {
+      panic(err)
+    }
+    
     fmt.Println("The database file was not found generating...")
+
+    pgpKey, err := keystore.GenerateKeys()
+    if err != nil {
+      panic(err)
+    }
+
+    if err := keystore.SavePGPKeys(keypass, pgpKey); err != nil {
+      panic(err)
+    }
   }
 }
 
